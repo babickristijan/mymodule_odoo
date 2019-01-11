098624cf37ebf31e5bc4d7eca4a5bb65feac3504
kbabic <kristijan.babic1@gmail.com>
2019-01-11 16:10:00 +0100

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'My module',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Write something',
+    'depends': ['web'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
